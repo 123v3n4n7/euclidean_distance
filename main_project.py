b529b7b12e6909217da0efbe1b7af4802db7aad7
123v3n4n7 <eugenezharikov@gmail.com>
2019-04-22 22:21:37 +0300

    load data from txt file

--- main_project.py
+++ main_project.py
@@ -1,55 +1,65 @@
 import csv
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
+import os
+import numpy
 
 
 def csv_reader():
     with open('vectors.csv', 'r') as file:
-        vector_matrix = []
+        vector_list = []
         reader = csv.reader(file)
         for line in reader:
             line_vector = []
             for i in line:
                 line_vector.append(float(i))
-            vector_matrix.append(line_vector)
-        return vector_matrix
+            vector_list.append(line_vector)
+        return vector_list
 
 
-def euclidean_distance(vector_array):
-    mx = vector_array
-    distances = []
-    couples = []
-    for i in range(len(mx)):
-        for j in range(i+1, len(mx)):
-            distances.append(distance.euclidean(mx[i], mx[j]))
-            couples.append([i, j])
-    max_dist = max(distances)
-    min_dist = min(distances)
-    couple_min = couples[distances.index(min(distances))][0], couples[distances.index(min(distances))][1]
-    couple_max = couples[distances.index(max(distances))][0], couples[distances.index(max(distances))][1]
-    hist_maker(distances)
-    return (max_dist, couple_max), (min_dist, couple_min)
+def euclidean_distance(vector_list):
+    with open('distance.txt', 'a') as f:
+        max_distance = 0.0
+        couple_max = ()
+        couple_min = ()
+        for i in range(len(vector_list)):
+            for j in range(i+1, len(vector_list)):
+                obj = distance.euclidean(vector_list[i], vector_list[j])
+                f.writelines('{}\n'.format(obj))
+                if obj >= max_distance:
+                    max_distance = obj
+                    couple_max = (i, j)
+        min_distance = max_distance
+        for i in range(len(vector_list)):
+            for j in range(i+1, len(vector_list)):
+                obj = distance.euclidean(vector_list[i], vector_list[j])
+                if obj < min_distance:
+                    min_distance = obj
+                    couple_min = (i, j)
+        return max_distance, couple_max, min_distance, couple_min
 
 
-def hist_maker(mx):
+def hist_maker(max_distance, couple_max, min_distance, couple_min):
     bins = []
-    x = float(0)
-    for i in range(int(round(max(mx))*10)):
+    x = 0.0
+    for i in range(int(round(max_distance) * 10)):
         bins.append(round(x, 1))
         x += 0.1
-    plt.yticks()
-    plt.xticks()
+    plt.style.use('ggplot')
     plt.ylabel('Частота')
-    plt.xlabel('Шаг')
-    plt.hist(mx, bins=bins, color='red')
+    plt.xlabel('Расстояния')
+    plt.hist(numpy.loadtxt('distance.txt'), bins=bins)
+    plt.title('Распределение расстояний')
+    print('Максимальное расстояние: ', max_distance, 'Номера пары векторов: ', couple_max, '\n',
+          'Минимальное расстояние: ', min_distance, 'Номера пары векторов: ', couple_min)
     plt.show()
-    #plt.savefig('graph.svg')
+    os.remove('distance.txt')
 
 
 def main():
-    matrix = csv_reader()
-    max_distance, min_distance = euclidean_distance(matrix)
-    print(max_distance, min_distance)
+    vector_list = csv_reader()
+    max_distance, couple_max, min_distance, couple_min = euclidean_distance(vector_list)
+    hist_maker(max_distance, couple_max, min_distance, couple_min)
 
 
 if __name__ == "__main__":
